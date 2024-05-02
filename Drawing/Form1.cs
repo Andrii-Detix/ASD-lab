e2aff2f8c7716b6ba7bf26db6b297a3b2f0426b4
Andrii-Detix <147273147+Andrii-Detix@users.noreply.github.com>
2024-05-02 12:10:36 +0300

    Update Form1.cs

--- Drawing/Form1.cs
+++ Drawing/Form1.cs
@@ -19,25 +19,33 @@ namespace Drawing
         private void textBox1_Start(object sender, EventArgs e)
         {
             g.Clear(Color.White);
-            double[] coefs;
+            double[] coefs = new[] { 1 - 0.05, 0, 0, -0.01, -0.005 };
             switch (comboBox1.SelectedIndex)
             {
                 case 0:
-                    coefs = new[] { 1 - 0.15, 0, 0, -0.01, -0.005 };
                     graph = new UndirectedGraph(int.Parse(textBox1.Text), coefs, 0.6f * Width, panel1.Top);
                     break;
                 case 1:
-                    coefs = new[] { 1 - 0.15, 0, 0, -0.01, -0.005 };
                     graph = new DirectedGraph(int.Parse(textBox1.Text), coefs, 0.6f * Width, panel1.Top);
+                    break;
+                case 2:
+                    graph = new WeightedGraph(int.Parse(textBox1.Text), coefs, 0.6f * Width, panel1.Top);
                     break;
             }
 
-            GraphDrawing.DrawMatrix(graph.Matrix, g, "Суміжності", new PointF(0.6f * Width, 0), new PointF(Width, panel1.Top));
+
+            GraphDrawing.DrawMatrix(graph.Matrix, g, "Суміжності", new PointF(0.6f * Width, 0),
+                new PointF(Width, panel1.Top));
             GraphDrawing.DrawGraph(graph, g);
             Form2 form2 = new Form2(graph);
-            form2.Show(); 
+            form2.Show();
             Form3 form3 = new Form3(graph);
             form3.Show();
+            if (graph is WeightedGraph weightedGraph)
+            {
+                Form4 form4 = new Form4(weightedGraph);
+                form4.Show();
+            }
         }
     }
 }
